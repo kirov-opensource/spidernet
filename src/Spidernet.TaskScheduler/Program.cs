07ef00463a4b8b8193ac81c7c87f843af38e848c
FatTiger <wangshenjie@wangshenjie.com>
2020-02-28 15:35:24 +0800

    initial project

---
+++ src/Spidernet.TaskScheduler/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Spidernet.TaskScheduler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Thread.Sleep(Timeout.Infinite);
+        }
+    }
+}
