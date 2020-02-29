9dc273ce616719bdf26777e36a64b570050eddaf
Fat Tiger <w1752738919@gmail.com>
2020-03-01 01:31:57 +0800

    add SymbolsEngine

---
+++ src/Spidernet.TaskScheduler/Extensions/SymbolsEngine.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Spidernet.TaskScheduler.Extensions {
+  public static class SymbolsEngine {
+
+    /// <summary>
+    /// Replace symbol with real value
+    /// </summary>
+    /// <param name="input">String with preprocessing symbols</param>
+    /// <param name="parameters">Parameters in JSON format</param>
+    /// <returns></returns>
+    public static string SymbolsPreprocess(string input, string parameters) {
+      var configuration = new ConfigurationBuilder().AddJsonStream(new MemoryStream(Encoding.UTF8.GetBytes(parameters))).Build();
+      return Regex.Replace(input, "", (match) => {
+        var section = configuration.GetSection(match.Value);
+        if (section.Exists()) {
+          return section.Value;
+        }
+        return match.Value;
+      });
+    }
+
+    /// <summary>
+    /// Replace symbol with real value
+    /// </summary>
+    /// <param name="input">String with preprocessing symbols</param>
+    /// <param name="configuration">Parameters in JSON format</param>
+    /// <returns></returns>
+    public static string SymbolsPreprocess(string input, IConfigurationRoot configuration) {
+      return Regex.Replace(input, "", (match) => {
+        var section = configuration.GetSection(match.Value);
+        if (section.Exists()) {
+          return section.Value;
+        }
+        return match.Value;
+      });
+    }
+  }
+}
